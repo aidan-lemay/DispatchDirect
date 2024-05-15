4b8223794a02e99d52f791bb0b4892c24b3e625d
Aidan LeMay <aquader14@gmail.com>
2024-05-15 13:12:37 -0400

    Adding a ton of new functions

--- api/routes/calls.js
+++ api/routes/calls.js
@@ -2,7 +2,64 @@ const express = require('express');
 const db = require('../db/db');
 const router = express.Router();
 
-// Get All Calls
+// -------------------- Helper Functions --------------------
+
+// Check Unit Status, returns "True" if unit is free or "False" if unit is busy
+function checkUnitStatus(unitID) {
+    return new Promise((resolve, reject) => {
+        var sql = `SELECT status FROM units WHERE unitID = ?`;
+        db.all(sql, [unitID], (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                reject(err);
+            } else {
+                if (rows[0]['status'] == "Unassigned") {
+                    resolve(true);
+                } else if (rows[0]['status'] == "Busy") {
+                    resolve(false);
+                }
+            }
+        });
+    });
+}
+
+// Validate CallID
+function checkCallID(callID) {
+    return new Promise((resolve, reject) => {
+        var sql = `SELECT COUNT(*) as "count" FROM calls WHERE callID = ?`;
+        db.all(sql, [callID], (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                reject(err);
+            } else {
+                if (rows[0]['count'] == 1) {
+                    resolve(true);
+                } else if (rows[0]['count'] == 0) {
+                    resolve(false);
+                }
+            }
+        });
+    });
+}
+
+// Get UnitID By CallID
+function unitFromCall(callID) {
+    return new Promise((resolve, reject) => {
+        var sql = `SELECT unit FROM calls WHERE callID = ?`;
+        db.all(sql, [callID], (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                reject(err);
+            } else {
+                resolve(rows[0]['unit']);
+            }
+        });
+    });
+}
+
+// -------------------- Route Definitions --------------------
+
+// Get Calls
 router.get('/', async (req, res) => {
     db.all('SELECT * FROM calls', (err, rows) => {
         if (err) {
@@ -13,8 +70,6 @@ router.get('/', async (req, res) => {
         }
     });
 });
-
-// Get Call By Run Number
 
 // Create New Call
 router.post('/', async (req, res) => {
@@ -86,11 +141,146 @@ router.post('/', async (req, res) => {
     }
 });
 
-// Update Call By Run Number
-router.put('/update', async (req, res) => {
+// Assign Unit By Call ID
+router.put('/unit', async (req, res) => {
+    try {
+        if (req.body) {
+            var { callID, unitID } = req.body;
 
+            // Validate CallID
+            checkCallID(callID)
+                .then(exists => {
+                    if (exists) {
+                        // Verify Unit Is Free
+                        checkUnitStatus(unitID)
+                            .then(isFree => {
+                                if (!isFree) {
+                                    res.status(400).send({ 'error': 'Unit Busy' });
+                                    res.end();
+                                }
+                                else {
+                                    // Set Unit To Busy
+                                    const sql = 'UPDATE units SET status = "Busy" WHERE unitID = ?';
+                                    db.run(sql, [unitID], function (err) {
+                                        if (err) {
+                                            console.error(err.message);
+                                            res.status(500).send('Internal server error');
+                                            res.end();
+                                        } else {
+                                            const sql = 'UPDATE calls SET unit = ?, status = "In-Progress" WHERE callID = ?';
+                                            db.run(sql, [unitID, callID], function (err) {
+                                                if (err) {
+                                                    console.error(err.message);
+                                                    res.status(500).send('Internal server error');
+                                                    res.end();
+                                                } else {
+                                                    const callID = this.lastID;
+                                                    res.status(201).send({ callID });
+                                                    res.end();
+                                                }
+                                            });
+                                        }
+                                    });
+                                }
+                            })
+                            .catch(err => {
+                                // Handle error
+                                console.error("Error:", err);
+                            });
+                    }
+                    else {
+                        res.status(400).send({ 'error': 'Call Does Not Exist' });
+                        res.end();
+                    }
+                })
+                .catch(err => {
+                    // Handle error
+                    console.error("Error:", err);
+                });
+        }
+        else {
+            res.status(400).send({ 'error': 'Missing Data' });
+            res.end();
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send({ 'error': error });
+        res.end();
+    }
 });
 
-// Delete Call By Run Number(Remove this call in prod?)
+// Close Call By ID
+router.put('/close', async (req, res) => {
+    try {
+        if (req.body) {
+            var { callID } = req.body;
+
+            // Validate CallID
+            checkCallID(callID)
+                .then(exists => {
+                    if (exists) {
+                        // Generate Data
+                        var close_time = new Date();
+
+                        const sql = 'UPDATE calls SET close_time = ?, status = "Closed" WHERE callID = ?';
+                        db.run(sql, [close_time, callID], function (err) {
+                            if (err) {
+                                console.error(err.message);
+                                res.status(500).send('Internal server error');
+                                res.end();
+                            } else {
+                                // Get UnitID From CallID
+                                unitFromCall(callID)
+                                    .then(unitID => {
+                                        if (unitID) {
+                                            // Set Unit To Free
+                                            const sql = 'UPDATE units SET status = "Free" WHERE unitID = ?';
+                                            db.run(sql, [unitID], function (err) {
+                                                if (err) {
+                                                    console.error(err.message);
+                                                    res.status(500).send('Internal server error');
+                                                    res.end();
+                                                } else {
+                                                    const callID = this.lastID;
+                                                    res.status(201).send({ callID });
+                                                    res.end();
+                                                }
+                                            });
+                                        }
+                                        else {
+                                            res.status(201).send({ callID });
+                                            res.end();
+                                        }
+                                    })
+                                    .catch(err => {
+                                        // Handle error
+                                        console.error("Error:", err);
+                                    });
+                            }
+                        });
+
+                    }
+                    else {
+                        res.status(400).send({ 'error': 'Call Does Not Exist' });
+                        res.end();
+                    }
+                })
+                .catch(err => {
+                    // Handle error
+                    console.error("Error:", err);
+                });
+        }
+        else {
+            res.status(400).send({ 'error': 'Missing Data' });
+            res.end();
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send({ 'error': error });
+        res.end();
+    }
+});
 
 module.exports = router;
--- api/routes/units.js
+++ api/routes/units.js
@@ -2,6 +2,29 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/db');
 
+// -------------------- Helper Functions --------------------
+
+// Returns "True" if there is an existing record with these parameters, and "False" if not.
+function checkUnique(name, contact) {
+    return new Promise((resolve, reject) => {
+        var sql = `SELECT COUNT(*) AS 'count' FROM units WHERE name = ? AND contact = ?`;
+        db.all(sql, [name, contact], (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                reject(err);
+            } else {
+                if (rows[0]['count'] == 0) {
+                    resolve(true);
+                } else if (rows[0]['count'] > 0) {
+                    resolve(false);
+                }
+            }
+        });
+    });
+}
+
+// -------------------- Route Definitions --------------------
+
 // Get All Units
 router.get('/', async (req, res) => {
     db.all('SELECT * FROM units', (err, rows) => {
@@ -23,34 +46,46 @@ router.post('/', async (req, res) => {
             name = name.toString();
             contact = contact.toString();
 
-            // Check to see if name and contact number are unique before adding to DB
-
-            // Validate input before adding to DB - name and contact are all user-generated.
-            if (name.length < 1) {
-                res.status(400).send({ 'error': 'Name Invalid' });
-                res.end();
-            }
-            else if (contact.length < 1) {
-                res.status(400).send({ 'error': 'Contact Number Invalid' });
-                res.end();
-            }
-            else {
-                // Create auto-generated data before adding to DB
-                var status = "Unassigned";
-
-                const sql = 'INSERT INTO units (name, contact, status) VALUES (?, ?, ?)';
-                db.run(sql, [name, contact, status], function (err) {
-                    if (err) {
-                        console.error(err.message);
-                        res.status(500).send('Internal server error');
-                        res.end();
-                    } else {
-                        const unitID = this.lastID;
-                        res.status(201).send({ unitID });
+            checkUnique(name, contact)
+                .then(isUnique => {
+                    // Check to see if name and contact number are unique before adding to DB
+                    if (!isUnique) {
+                        res.status(400).send({ 'error': 'Unit Exists' });
                         res.end();
                     }
+                    else {
+                        // Validate input before adding to DB - name and contact are all user-generated.
+                        if (name.length < 1) {
+                            res.status(400).send({ 'error': 'Name Invalid' });
+                            res.end();
+                        }
+                        else if (contact.length < 1) {
+                            res.status(400).send({ 'error': 'Contact Number Invalid' });
+                            res.end();
+                        }
+                        else {
+                            // Create auto-generated data before adding to DB
+                            var status = "Unassigned";
+
+                            const sql = 'INSERT INTO units (name, contact, status) VALUES (?, ?, ?)';
+                            db.run(sql, [name, contact, status], function (err) {
+                                if (err) {
+                                    console.error(err.message);
+                                    res.status(500).send('Internal server error');
+                                    res.end();
+                                } else {
+                                    const unitID = this.lastID;
+                                    res.status(201).send({ unitID });
+                                    res.end();
+                                }
+                            });
+                        }
+                    }
+                })
+                .catch(err => {
+                    // Handle error
+                    console.error("Error:", err);
                 });
-            }
         }
         else {
             res.status(400).send({ 'error': 'Missing Data' });
