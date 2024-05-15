b71421d0eda5e16f84c89ec64befe4d83ad98283
Aidan LeMay <aquader14@gmail.com>
2024-05-15 11:19:01 -0400

    Creating tables

--- api/db/db.js
+++ api/db/db.js
@@ -1,31 +1,56 @@
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('./dispatch.db', (err) => {
-    createTable();
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to the dispatch database.');
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to database "dispatch".');
+  createCalls();
+  createUnits();
 });
 
-function createTable(db) {
+function createCalls() {
+  try {
     db.exec(`
-    CREATE TABLE calls
-    (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      run_no VARCHAR(50) NOT NULL,
-      name   VARCHAR(50) NOT NULL,
-      phone   VARCHAR(50) NOT NULL,
-      bib INTEGER NOT NULL,
-      location VARCHAR(100) NOT NULL,
-      complaint VARCHAR(100) NOT NULL,
-      open_time TIMESTAMP NOT NULL,
-      close_time TIMESTAMP,
-      unit VARCHAR(100) NOT NULL,
-      status VARCHAR(100) NOT NULL,
-      notes VARCHAR(100)
-    );
-  `);
+      CREATE TABLE IF NOT EXISTS calls (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        run_no VARCHAR(50) NOT NULL,
+        name VARCHAR(50) NOT NULL,
+        phone VARCHAR(50) NOT NULL,
+        bib INTEGER NOT NULL,
+        location VARCHAR(100) NOT NULL,
+        complaint VARCHAR(100) NOT NULL,
+        open_time TIMESTAMP NOT NULL,
+        close_time TIMESTAMP,
+        unit VARCHAR(100) NOT NULL,
+        status VARCHAR(100) NOT NULL,
+        notes VARCHAR(100)
+      );
+    `);
+    console.log('Table "calls" created successfully');
+  }
+  catch (error) {
+    console.error(error);
+  }
 }
+
+function createUnits() {
+  try {
+    db.exec(`
+      CREATE TABLE IF NOT EXISTS units (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(100) NOT NULL,
+        status VARCHAR(50) NOT NULL,
+        contact VARCHAR(100) NOT NULL
+      );
+    `);
+    console.log('Table "units" created successfully');
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
+
 
 module.exports = db;
